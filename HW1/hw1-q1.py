e84821fcc43728fe1491d7e5c0b51c5a238023aa
diogojpa99 <diogo.jpa.99@gmail.com>
2022-12-18 17:20:38 +0000

    Do Q2

--- HW1/hw1-q1.py
+++ HW1/hw1-q1.py
@@ -11,7 +11,6 @@ import matplotlib.pyplot as plt
 
 import utils
 
-
 def configure_seed(seed):
     os.environ["PYTHONHASHSEED"] = str(seed)
     random.seed(seed)
@@ -19,10 +18,27 @@ def configure_seed(seed):
 
 def softmax(z):
     """
-            Softmax function.
-        Implemented by group 40.
+    Softmax function.
+    Implemented by the group.
     """
-    return np.exp(z) / np.sum(np.exp(z))
+    exp_z = np.exp(z - np.max(z))
+    return exp_z / np.sum(exp_z)
+
+def relu(z):
+    """
+    RELU activation function for mlp with one hidden layer.
+    Implemented by the group.
+    """
+    return np.maximum(0,z)
+
+def relu_prime(z):
+    """
+    Derivative of the RELU activation function for the whole array.
+    Implemented by the group.
+    """
+    z[np.where(z>0)] = 1
+    z[np.where(z<=0)] = 0
+    return z
 
 class LinearModel(object):
     def __init__(self, n_classes, n_features, **kwargs):
@@ -64,6 +80,7 @@ class Perceptron(LinearModel):
         # Q1.1a
         # (1) Prediction
         y_hat = self.predict(x_i)
+        
         # (2) Update weigths
         if y_hat != y_i: 
             self.W[y_i] += x_i #increase weight of gold class 
@@ -80,13 +97,17 @@ class LogisticRegression(LinearModel):
         # Q1.1b
         # (Init) Reshap input 
         x_i = x_i.reshape(np.size(x_i,0),1)
+        
         # (1) Class scores
         class_scores = self.W.dot(x_i)
-        # (2) Class probabilities (softmax)
+        
+        # (2) Class probabilities 
         class_probs = softmax(class_scores)
+        
         # (3) One-hot encoding of the output vector
         y_one_hot = np.zeros((np.size(self.W,0),1))  
         y_one_hot[y_i] = 1   
+        
         # (4) SGD update
         self.W += learning_rate * np.outer((y_one_hot - class_probs), x_i.T)
 
@@ -97,18 +118,27 @@ class MLP(object):
     # in main().
     def __init__(self, n_classes, n_features, hidden_size, layers):
         # Initialize an MLP with a single hidden layer.
+        
         # (1) Initialize bias with zero vectors
-        self.b1 = np.zeros((hidden_size,0))
-        self.b2 = np.zeros((n_classes,0))
+        self.b1 = np.zeros((hidden_size,1))
+        self.b2 = np.zeros((n_classes,1))
+        
         # (2) Initialize weights matrices with random values
         self.W1 = np.random.normal(loc=0.1, scale=0.1, size=(hidden_size,n_features))
-        self.W2 = np.random.normal(loc=0.1, scale=0.1, size=(n_classes,n_features))
+        self.W2 = np.random.normal(loc=0.1, scale=0.1, size=(n_classes,hidden_size))
 
     def predict(self, X):
         # Compute the forward pass of the network. At prediction time, there is
         # no need to save the values of hidden nodes, whereas this is required
         # at training time.
-        raise NotImplementedError
+        
+        # Simply use the feedforward step for the output
+        z1 = self.W1.dot(X.T) + self.b1
+        h1 = relu(z1)
+        z2 = self.W2.dot(h1) + self.b2
+        h2 = softmax(z2)
+        
+        return np.argmax(h2, axis = 0)
 
     def evaluate(self, X, y):
         """
@@ -122,13 +152,37 @@ class MLP(object):
         return n_correct / n_possible
 
     def train_epoch(self, X, y, learning_rate=0.001):
+        # For each instance
         for x_i,y_i in zip(X,y):
             
-            # (Init) Reshape the input:
+            # (Init) Reshaping the input:
             x_i = x_i.reshape(np.size(x_i,0),1)
             
+            # (1) One-hot encoding of the output vector
+            y_one_hot = np.zeros((np.size(self.W2,0),1))  
+            y_one_hot[y_i] = 1   
+            
+            # (2) Feedfoward propagation step
+            z1 = self.W1.dot(x_i) + self.b1
+            h1 = relu(z1)
+            z2 = self.W2.dot(h1) + self.b2
+            h2 = softmax(z2)
+            
+            # (3) Back propagation step
+            z2_grad = h2 - y_one_hot
+            w2_grad = z2_grad.dot(h1.T)
+            b2_grad = z2_grad
+            z1_grad = self.W2.T.dot(z2_grad) * relu_prime(z1)
+            w1_grad = z1_grad.dot(x_i.T)
+            b1_grad = z1_grad
+            
+            # (4) Updating the weights using SGD
+            self.W1 -= learning_rate * w1_grad
+            self.b1 -= learning_rate * b1_grad
+            self.W2 -= learning_rate * w2_grad
+            self.b2 -= learning_rate * b2_grad
 
-
+            
 
 def plot(epochs, valid_accs, test_accs):
     plt.xlabel('Epoch')
@@ -192,14 +246,8 @@ def main():
         )
         valid_accs.append(model.evaluate(dev_X, dev_y))
         test_accs.append(model.evaluate(test_X, test_y))
-
-    # (New)
-    # Print the accuracies for the last epoch
-    print("Validation set acc. for last epoch:", valid_accs[-1])
-    print("Test set acc. for last epoch:", test_accs[-1])
-    # Print the accuracies for the first epoch
-    print("Validation set acc. for first epoch:", valid_accs[0])
-    print("Test set acc. for first epoch:", test_accs[0])
+        # Implemented by the group
+        print('Valid acc:', valid_accs[-1], '| Test acc:', test_accs[-1]) 
 
     # plot
     plot(epochs, valid_accs, test_accs)
